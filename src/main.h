128c5c1b8781f15a3d9f365cc2c6ca8b316c750d
chenyuan <chenyuan@thinkcom.com.cn>
2018-12-06 11:13:53 +0800

    判断返回值是否正常的宏

--- src/main.h
+++ src/main.h
@@ -2,11 +2,23 @@
 #define	__MIAN_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 typedef int INT32;
 
 typedef char CHAR;
+
+#define GOS_ASSERT(cond) if (!(cond)) \
+                      {\
+                        printf("\r\nAssertion Failed:"#cond", file %s, line %d, pid %d\r\n", __FILE__, __LINE__, getpid()); \
+                        abort();\
+                      }\
+                      else {}
 
 
 #endif
